c98cbb3928c301f2287a372cb694ba7048bb85bc
Aditya <adityavishal903@gmail.com>
2022-11-03 09:51:57 +0530

    02

---
+++ lib/homePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "College Space";
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("College Space"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Wellcome To College Space "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
