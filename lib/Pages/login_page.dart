78a2a0556ad3fda3355abf448a9e6d16b2268461
Aditya <adityavishal903@gmail.com>
2022-11-07 18:31:59 +0530

    Day04

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
+// ignore: use_key_in_widget_constructors
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/login.png",
+          fit: BoxFit.cover,
+        ),
+        const Text(
+          "Welcome",
           style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
+            fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
-        ),
-      ),
-    );
+        )
+      ],
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:myawesomeapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_catalog/pages/login_page.dart';
 
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -11,17 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.deepPurple,
+      // ),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/home",
       routes: {
-        //"/": (context) => LoginPage(),
+        "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
-        //"/login": (context) => LoginPage()
+        "/login": (context) => LoginPage(),
       },
+      // initialRoute: "/login",
+      // routes: {
+      //   "/login": (context) => LoginPage(),
+      //   "/home": (context) => HomePage(),
+      // },
+      home: LoginPage(),
     );
   }
 }
